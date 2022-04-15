8ede3ae437c2ba2fb8cd789e81d4899c68137126
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2022-04-15 14:54:19 -0300

    :sparkles: classe modal e métodos de abertura/fechamento

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+class Modal {
+    constructor(modal, overlay, closeBtn) {
+        this.overlay = overlay;
+        this.modal = modal;
+        this.closeBtn = closeBtn;
+        this.isShowing = false;
+        this.openers = [];
+        this.overlay.addEventListener('click', () => {this.show(false)});
+        this.closeBtn.addEventListener('click', () => {this.show(false)});
+    }
+
+    show(show) {
+        if (this.isShowing !== show) {
+            const action = show ? "remove" : "add";
+            this.overlay.classList[action]('hidden');
+            this.modal.classList[action]('hidden');
+            this.isShowing = show;
+        }
+    }
+
+    addOpener(button) {
+        if (this.openers.includes(button)) {
+            return false;
+        }
+        button.addEventListener('click', () => {this.show(true)});
+    }
+}
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const modalController = new Modal(modal, overlay, closeBtn);
+const buttons = document.querySelectorAll(".show-modal");
+buttons.forEach(button => {
+    modalController.addOpener(button);
+});
